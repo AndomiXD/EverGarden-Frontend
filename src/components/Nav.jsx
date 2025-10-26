b9bdc03026df56b480c6cdf8016420ebdf3b40a7
shooqbinzaiman <shooqshowqi@gmail.com>
2025-10-26 14:39:43 +0300

    components/Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,1 +1,34 @@
+import { Link } from 'react-router-dom'
 
+const Nav = ({ user, handleLogout}) => {
+  let userOptions
+
+  if(user){
+    userOptions = (
+      <>
+      <h3>Welcome {user.name}!</h3>
+      <Link to="/garden">My Garden</Link>
+      <Link onClick={handleLogOut} to="/">
+      Sign Out
+      </Link>
+      </>
+    )
+  }
+
+  const publicOptions = (
+    <>
+    <Link to="/">Home</Link>
+    <Link to="/signup">Sign Up</Link>
+    <Link to="/login">Login</Link>
+   </>
+  )
+  return (
+    <header>
+      <nav>
+        {user? userOptions : publicOptions }
+      </nav>
+    </header>
+  )
+}
+
+export default Nav
