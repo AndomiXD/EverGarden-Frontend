0a9aa05b4cb6d7c1969ffc61fc21402844f3b2e7
AndomiXD <vintin4some@gmail.com>
2025-10-29 16:23:50 +0300

    code polishing#1

--- src/components/GardenGrid.jsx
+++ src/components/GardenGrid.jsx
@@ -1,14 +1,17 @@
 import GardenPosition from "./GardenPosition"
 
 const GardenGrid = ({ garden, onHarvest, onRemove, onDropSeed }) => {
-  const totalSlots = 12 // we could add a new field in the model for this
-
   return (
     <div className="garden-grid">
       <h2 className="garden-title">{garden.name}</h2>
       <div className="garden-grid-layout">
-        {Array.from({ length: totalSlots }).map((_, position) => {
-          const plant = garden.plants?.find((p) => p.position === position)
+        {/* https://stackoverflow.com/questions/68737171/how-to-create-a-fixed-length-array-in-reactjs */}
+
+        {/* we could add a new field in the model for the length */}
+        {Array.from({ length: 12 }).map((_, position) => {
+          const plant = garden.plants?.find(
+            (plant) => plant.position === position
+          )
           return (
             <GardenPosition
               key={position}
