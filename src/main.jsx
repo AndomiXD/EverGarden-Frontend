632c77d12dade341f8aa319de88a9d13170af848
AndomiXD <vintin4some@gmail.com>
2025-10-30 07:54:50 +0300

    add main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
-import React from "react"
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import App from "./App.jsx"
 
-createRoot(document.querySelector('#root')).render(
+createRoot(document.querySelector("#root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
